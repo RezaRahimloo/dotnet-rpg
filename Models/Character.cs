5367427bc5878963dd6ddc2839c6370575a33fc0
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-08-20 10:39:55 +0430

    Added Fighting properties to characters

--- Models/Character.cs
+++ Models/Character.cs
@@ -16,5 +16,8 @@ namespace dotnet_rpg.Models
         //[ForeignKey("CharacterId")]
         public Weapon Weapon { get; set; }
         public List<Skill> Skills { get; set; }
+        public int Fights { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
     }
 }
