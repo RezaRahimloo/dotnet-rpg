9771f7a98aee10c04b2640aa4116f346b9b9fe43
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-08-20 11:40:05 +0430

    Now two characters can fight and the state of their fight will be saved

--- Controllers/FightController.cs
+++ Controllers/FightController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Models;
 using dotnet_rpg.Services.FightService;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -18,6 +20,11 @@ namespace dotnet_rpg.Controllers
         public FightController(IFightService fightService)
         {
             _fightService = fightService;
+        }
+        [HttpPost("WeaponAttack")]
+        public async Task<ActionResult<ServiceResponse<AttackResultDto>>> WeaponAttack(WeaponAttackDto request)
+        {
+            return Ok(await _fightService.WeaponAttackAsync(request));
         }
     }
 }
--- Services/FightService/IFightService.cs
+++ Services/FightService/IFightService.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Services.FightService
 {
     public interface IFightService
     {
-        
+        Task<ServiceResponse<AttackResultDto>> WeaponAttackAsync(WeaponAttackDto request);
     }
 }
