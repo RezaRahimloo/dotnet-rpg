a659af1d83bae11ca1091ee394f741a47bde5048
Reza Rahimloo <mr.rezarahimlou@gmail.com>
2022-08-20 08:26:07 +0430

    Added Data seeding to skills

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -14,6 +14,16 @@ namespace dotnet_rpg.Data
             var a = 2*2;
             Console.WriteLine(a);
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Skill>().HasData(
+                new Skill { Id = 1, Name = "Fire ball", Damage = 30},
+                new Skill { Id = 2, Name = "Grasp heart", Damage = 10000},
+                new Skill { Id = 3, Name = "Black Hole", Damage = 400},
+                new Skill { Id = 4, Name = "Blink", Damage = 0}
+            );
+        }
 
         public DbSet<User> Users { get; set; }
         public DbSet<Character> Characters { get; set; }
---
+++ Migrations/20220820035433_skillSeeding.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet_rpg.Migrations
+{
+    public partial class skillSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Skills",
+                columns: new[] { "Id", "Damage", "Name" },
+                values: new object[,]
+                {
+                    { 1, 30, "Fire ball" },
+                    { 2, 10000, "Grasp heart" },
+                    { 3, 400, "Black Hole" },
+                    { 4, 0, "Blink" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Skills",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
